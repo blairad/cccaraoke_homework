cb8e36ffb846b189a2a0e765e61ef94bd86559ca
Andrew Blair <blair.d.andrew@gmail.com>
2019-04-13 12:20:44 +0100

    make_a_room_works

--- room.rb
+++ room.rb
@@ -1,10 +1,21 @@
+
+
 class Room
 
-  attr_reader :room
+  attr_reader :room, :capacity, :song
 
-  def initialize(room)
+  def initialize(room, capacity)
     @room = room
+    @capacity = capacity
   end
 
 
+
+
+  # def check_in_guest(name,capacity)
+  #   @capacity += room.name
+  # end
+
+
 end
+#
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -1,19 +1,27 @@
+require('pry')
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../room.rb")
 require_relative("../guest.rb")
 require_relative("../song.rb")
 
+
 class RoomTest < MiniTest:: Test
 
   def setup
-    @room1 = Room.new("No. 1")
-    @room2 = Room.new("No. 2")
+    @room = Room.new("No. 1", 0)
+    @capacity = nil
+    @capacity_max = 5
   end
 
-  def test_make_aRoom
-    assert_equal("No. 1", @room1.room)
+  def test_make_a_room
+    assert_equal("No. 1", @room.room)
   end
 
+  # def test_check_in_guest_to_room
+  #   @room.check_in_guest(@guest1)
+  #   assert_equal(1, @room.name.count)
+  #   binding.pry
+  # end
 
 end
